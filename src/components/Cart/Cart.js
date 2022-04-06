3db9a7e925e8283879d4fe235047f941f3721eaa
ratulinspace <i.shahriarislam.ratul@gmail.com>
2022-04-06 22:19:07 +0600

    CONDITIONAL RENDERING OPTIONS

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,9 +2,38 @@ import React from 'react';
 import "./Cart.css";
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+
+    /* CONDITIONAL RENDERING OPTIONS */
+
+
+    /* // 1. ELEMENT VARIABLE
+    let command;
+    if (cart.length === 0) {
+        command = <p>Please add some items</p>
+    }
+    else if (cart.length === 1) (
+        command = <p>Please, add more item ! </p>
+    )
+    else {
+        command = <p>Thanks for adding Items</p>
+    } */
+
+
+    /* // 2. TERNARY OPERATION
+    {cart.length !== 4 ? <p>Keep Adding</p> : <button>Remove all</button>} */
+
+
+    // 3. && OPERATOR (shorthand)
+    /* {cart.length === 3 && <div className='orange'>
+                <h3>Trigonal</h3>
+                <p>#3 Items</p>
+            </div>} */
+
+
     return (
         <div>
-            <h3>Items Seleceted: {cart.length}</h3>
+            <h3>Items Selected: {cart.length}</h3>
+
             {
                 cart.map(tShirt =>
                     <p>{tShirt.name}
@@ -12,6 +41,13 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                     </p>
                 )
             }
+            {/* {command} */}
+            {/* {cart.length !== 4 ? <p>Keep Adding</p> : <button>Remove all</button>} */}
+            {/* {cart.length === 3 && <div>
+                <h3>Trigonal</h3>
+                <p>#3 Items</p>
+            </div>} */}
+            {/* {cart.length === 0 || <p>YOU ARE B U Y I N G</p>} */}
         </div>
     );
 };
